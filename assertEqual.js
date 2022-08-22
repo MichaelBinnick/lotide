b5ac3ceebbf8ac51d0c9a6222efc94ede7a75826
Michael Binnick <mbinnick@live.ca>
2022-08-22 18:42:56 +0000

    implemented assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log('😀😀😀Assertion Passed: ' + actual + ' === ' + expected) : console.log('🤢🤢🤢Assertion Failed: ' + actual + ' !== ' + expected);
+};
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(true, 1);
+assertEqual(true, 1 === 1);
+assertEqual('10', 10);
+assertEqual('hi', 'hi');
+assertEqual('hi', 'hello');
+assertEqual(10, 15);
