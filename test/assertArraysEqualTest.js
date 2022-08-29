c0f7e91d5ae08b82c97461296ed94e09e7876763
Michael Binnick <mbinnick@live.ca>
2022-08-29 20:09:18 +0000

    cleaned up existing test, commented out 'assert' functions to cleanup test output and also because I'm using Chai asserts instead now

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,5 @@
-const assertArraysEqual = require('../assertArraysEqual.js');
+// const assertArraysEqual = require('../assertArraysEqual.js');
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([2, 2, 3], [1, 2, 3]);
-assertArraysEqual(['hi'], ['hi']);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([2, 2, 3], [1, 2, 3]);
+// assertArraysEqual(['hi'], ['hi']);
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,10 +1,10 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(true, 1);
-assertEqual(true, 1 === 1);
-assertEqual('10', 10);
-assertEqual('hi', 'hi');
-assertEqual('hi', 'hello');
-assertEqual(10, 15);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual(true, 1);
+// assertEqual(true, 1 === 1);
+// assertEqual('10', 10);
+// assertEqual('hi', 'hi');
+// assertEqual('hi', 'hello');
+// assertEqual(10, 15);
