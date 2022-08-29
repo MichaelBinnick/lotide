07d940191b27b92fd59614b1760ae9307f354edf
Michael Binnick <mbinnick@live.ca>
2022-08-29 14:01:05 +0000

    refactored tailTest.js and middleiddleTest.js to utilize mocha/chai frameworks

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual.js');
+const assert = require('chai').assert;
 const middle = require('../middle.js');
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // should pass
-assertArraysEqual(middle([1, 3, 37]), [3]); // should pass
-assertArraysEqual(middle([2, 7, 31, 46]), [7, 46]); // should fail
-assertArraysEqual(middle(['hi', 'hello', 'yo']), ['hello']); // should pass
-assertArraysEqual(middle(['you', 'rule', 'at', 'coding']), ['rule', 'at']); // should pass
+describe("#middle", () => {
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [3] for [1, 3, 37]", () => {
+    assert.deepEqual(middle([1, 3, 37]), [3]);
+  });
+  it("returns [7, 31] for [2, 7, 31, 46]", () => {
+    assert.deepEqual(middle([2, 7, 31, 46]), [7, 31]);
+  });
+  it("returns ['hello'] for ['hi', 'hello', 'yo']", () => {
+    assert.deepEqual(middle(['hi', 'hello', 'yo']), ['hello']);
+  });
+  it("returns ['rule', 'at'] for ['you', 'rule', 'at', 'coding']", () => {
+    assert.deepEqual(middle(['you', 'rule', 'at', 'coding']), ['rule', 'at']);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,17 @@
 const tail = require('../tail.js');
-const assertEqual = require('../assertEqual.js');
+const assert = require('chai').assert;
 
-let testArray = [1, 2, 3];
-assertEqual(tail(testArray).length, testArray.length - 1);
-assertEqual(testArray[1], tail(testArray)[0]);
-
-let words = ['hi', 'yo', 'sup'];
-tail(words);
-assertEqual(words.length, 3);
-
-let emptyArrayTest = [1];
-assertEqual(tail(emptyArrayTest).length, 0);
-
-let superEmptyArrayTest = [];
-assertEqual(tail(superEmptyArrayTest).length, 0);
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns ['yo', 'sup'] for ['hi', 'yo', 'sup']", () => {
+    assert.deepEqual(tail(['hi', 'yo', 'sup']), ['yo', 'sup']);
+  });
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
