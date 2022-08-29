ba3778b9c22e8b7f6ce385c0581f6f5cca223ca7
Michael Binnick <mbinnick@live.ca>
2022-08-29 19:38:16 +0000

    refactored flatten to export functiotionality, wrote flattenTest.js

--- flatten.js
+++ flatten.js
@@ -1,11 +1,3 @@
-const eqArrays = (arrayOne, arrayTwo) => {
-  return arrayOne.toString() === arrayTwo.toString();
-};
-
-const assertArraysEqual = (arrayOne, arrayTwo) => {
-  eqArrays(arrayOne, arrayTwo) ? console.log(`😀😀😀Assertion Passed`) : console.log(`🤢🤢🤢Assertion Failed`);
-};
-
 const flatten = array => {
   let output = [];
   for (let element of array) {
@@ -14,6 +6,4 @@ const flatten = array => {
   return output;
 };
 
-assertArraysEqual(flatten([1, 2, [1, 2]]), [1, 2, 1, 2]);
-assertArraysEqual(flatten(['candy', 'apple', ['chocolate', 'twix']]), ['candy', 'apple', 'chocolate', 'twix']);
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6, 7], 8]), [1, 2, 3, 4, 5, 6, 7, 8])
+module.exports = flatten;
---
+++ test/flattenTest.js
@@ -0,0 +1,18 @@
+const assert = require('chai').assert;
+const flatten = require('../index.js').flatten;
+
+// assertArraysEqual(flatten([1, 2, [1, 2]]), [1, 2, 1, 2]);
+// assertArraysEqual(flatten(['candy', 'apple', ['chocolate', 'twix']]), ['candy', 'apple', 'chocolate', 'twix']);
+// assertArraysEqual(flatten([1, 2, [3, 4], 5, [6, 7], 8]), [1, 2, 3, 4, 5, 6, 7, 8])
+
+describe("#flatten on the happy path", () => {
+  it("returns [1, 2, 1, 2] for ([1, 2, [1, 2]])", () => {
+    assert.deepEqual(flatten([1, 2, [1, 2]]), [1, 2, 1, 2]);
+  });
+  it("returns ['candy', 'apple', 'chocolate', 'twix'] for (['candy', 'apple', ['chocolate', 'twix']])", () => {
+    assert.deepEqual(flatten(['candy', 'apple', ['chocolate', 'twix']]), ['candy', 'apple', 'chocolate', 'twix']);
+  });
+  it("returns [1, 2, 3, 4, 5, 6, 7, 8] for ([1, 2, [3, 4], 5, [6, 7], 8])", () => {
+    assert.deepEqual(flatten([1, 2, [3, 4], 5, [6, 7], 8]), [1, 2, 3, 4, 5, 6, 7, 8]);
+  });
+});
